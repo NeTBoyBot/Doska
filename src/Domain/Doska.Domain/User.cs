7e180ce9225450493d124a97e2e5d0ac6bb9a7f7
NeTBoyBot <toolumini@mail.ru>
2023-04-06 17:42:48 +0300

    make identity user

--- src/Domain/Doska.Domain/User.cs
+++ src/Domain/Doska.Domain/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Doska.Domain
 {
-    public class User
+    public class User : Microsoft.AspNetCore.Identity.IdentityUser
     {
         public Guid Id { get; set; }
 
